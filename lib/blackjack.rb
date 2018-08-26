f608e0e1dccba138ed653a4d5afffd9297d3afc7
Shuo Wang <gken90@gmail.com>
2018-08-26 13:11:39 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,44 +1,56 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  user_input = gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  hand = []
+  hand << deal_card
+  hand << deal_card
+  sum_of_cards = hand[0] + hand[1]
+  display_card_total(sum_of_cards)
+  return sum_of_cards
 end
 
-def hit?
-  # code hit? here
+
+def hit?(card_total)
+  prompt_user
+  input = get_user_input
+  if input == 's'
+    return card_total
+  elsif input == 'h'
+    card_total += deal_card
+  end
 end
 
-def invalid_command
-  # code invalid_command here
-end
+# def invalid_command
+#   # code invalid_command here
+# end
 
 #####################################################
 # get every test to pass before coding runner below #
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  initial_round
 end
-    
